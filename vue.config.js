16fa9d6ac39eb3842c89130e99e8ac1c9436c376
htet-waiyan <h.waeyan@gmail.com>
2020-03-01 01:44:58 +0800

    feat: pwa vue config and icons

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pwa: {
+    name: 'Spllit',
+    manifestOptions: {
+      name: 'Spllit',
+      short_name: 'Spllit',
+      start_url: '.',
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32-spllit.png',
+      favicon16: 'img/icons/favicon-16x16-spllit.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-spllit.png',
+    },
+  },
+};
